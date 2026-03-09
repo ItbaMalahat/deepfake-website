3c2b9d9005f60112024d672c95b3c1be48616cc3
Itba Malahat <100957307+ItbaMalahat@users.noreply.github.com>
2026-03-09 15:40:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const toggle=document.querySelector('.menu-toggle');const nav=document.querySelector('.main-nav');if(toggle&&nav){toggle.addEventListener('click',()=>nav.classList.toggle('open'));}
+const menuToggle = document.querySelector(".menu-toggle");
+const mainNav = document.querySelector(".main-nav");
+
+if (menuToggle && mainNav) {
+  menuToggle.addEventListener("click", () => {
+    mainNav.classList.toggle("open");
+  });
+}
